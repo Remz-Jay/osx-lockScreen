f1d721317a6e251fb33ec295db7330d5b6dc2bef
Remco Overdijk <remco@Prosperpine.local>
2012-01-23 21:39:24 +0100

    Initial Commit

---
+++ lockScreen/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  lockScreen
+//
+//  Created by Remco Overdijk on 23-01-12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
